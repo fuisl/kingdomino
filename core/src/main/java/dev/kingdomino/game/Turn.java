6f3368def7950614c05b67096e68cbb8e1aef4d5
fuisl <135719107+fuisl@users.noreply.github.com>
2024-12-19 16:34:25 +0100

    modify and add some more components.

--- core/src/main/java/dev/kingdomino/game/Turn.java
+++ core/src/main/java/dev/kingdomino/game/Turn.java
@@ -1,4 +1,35 @@
 package dev.kingdomino.game;
 
-public abstract class Turn {
+import java.util.ArrayList;
+
+public class Turn {
+    private ArrayList<Player> players;
+    private ArrayList<Domino> dominos;
+    private int currentPlayerIndex;
+
+    public Turn(ArrayList<Player> players, ArrayList<Domino> dominos) {
+        this.players = players;
+        this.dominos = dominos;
+        this.currentPlayerIndex = 0;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public ArrayList<Domino> getDominos() {
+        return dominos;
+    }
+
+    public Player getCurrentPlayer() {
+        return players.get(currentPlayerIndex);
+    }
+
+    public void setNextPlayer() {
+        currentPlayerIndex = currentPlayerIndex < players.size() ? (currentPlayerIndex + 1) : null;
+    }
+
+    public void setPreviousPlayer() {
+        currentPlayerIndex = currentPlayerIndex > 0 ? (currentPlayerIndex - 1) : null;
+    }
 }
