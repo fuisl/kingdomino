1b93277ed7ae9e8534146637081eef423ef40e3b
fuisl <135719107+fuisl@users.noreply.github.com>
2024-12-19 15:25:38 +0100

    feat(main logic): init core game.

--- core/src/main/java/dev/kingdomino/game/Kingdomino.java
+++ core/src/main/java/dev/kingdomino/game/Kingdomino.java
@@ -1,5 +1,12 @@
 package dev.kingdomino.game;
 
 public class Kingdomino {
-    
+    private int numberOfPlayers; // 2 - 4 players
+    private int numberOfTiles; // number of tiles in the game (4 players = 48, 3 players = 36, 2 players = 24)
+
+    // setup
+    public Kingdomino() {
+    }
+
+    CardDeck deck = new CardDeck(0); // create a new deck of cards
 }
