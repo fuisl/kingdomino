5c3a77a1f636ec934c4ecab4ad11bcf56d4a0116
fuisl <135719107+fuisl@users.noreply.github.com>
2024-12-19 15:25:09 +0100

    feat(board): add attributes of board.

--- core/src/main/java/dev/kingdomino/game/Board.java
+++ core/src/main/java/dev/kingdomino/game/Board.java
@@ -1,5 +1,37 @@
 package dev.kingdomino.game;
 
-public interface Board {
+public class Board {
+    private Tile[][] land;
+    private int totalPoint;
+    private int additionalPoint;
 
+    public Board() {
+        land = new Tile[9][9];
+        totalPoint = 0;
+        additionalPoint = 0;
+    }
+
+    public void setTile(Tile tile, int x, int y) {
+        land[x][y] = tile;
+    }
+
+    public Tile getTile(int x, int y) {
+        return land[x][y];
+    }
+
+    public int getTotalPoint() {
+        return totalPoint;
+    }
+
+    public void setTotalPoint(int totalPoint) {
+        this.totalPoint = totalPoint;
+    }
+
+    public int getAdditionalPoint() {
+        return additionalPoint;
+    }
+
+    public void setAdditionalPoint(int additionalPoint) {
+        this.additionalPoint = additionalPoint;
+    }
 }
