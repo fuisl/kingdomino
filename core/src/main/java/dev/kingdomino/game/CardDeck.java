1934111b55ac8063384acc53222f2ab2fd4b2a9b
fuisl <135719107+fuisl@users.noreply.github.com>
2024-12-19 14:47:19 +0100

    feat(card deck): introduce seed for random generator.

--- core/src/main/java/dev/kingdomino/game/CardDeck.java
+++ core/src/main/java/dev/kingdomino/game/CardDeck.java
@@ -2,21 +2,32 @@ package dev.kingdomino.game;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;  // TODO: use tools from LibGDX instead of Java Collections
+import java.util.List; // TODO: use tools from LibGDX instead of Java Collections
 
-public abstract class CardDeck {
+public class CardDeck {
     // Common properties and methods for all card decks
     protected List<Domino> deck;
+    protected int seed;
 
     public CardDeck() {
         this.deck = new ArrayList<>();
         initializeDeck();
     }
+    
+    public CardDeck(int seed) {
+        this.seed = seed;
+        this.deck = new ArrayList<>();
+        initializeDeck();
+    }
 
-    protected abstract void initializeDeck();  // Initialize the deck with the appropriate cards
+    protected void initializeDeck() {
+        for (int i = 0; i < 48; i++) {
+            // TODO: init the deck with the appropriate cards
+        }
+    }; // Initialize the deck with the appropriate cards
 
     public void shuffle() {
-        Collections.shuffle(deck);
+        Collections.shuffle(deck, new java.util.Random(seed));
     }
 
     public Domino drawCard() {
