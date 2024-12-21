f5a7d267c0cebf804de1c484a825439ba12841fe
fuisl <135719107+fuisl@users.noreply.github.com>
2024-12-21 22:23:07 +0100

    test: handling some movement of the Dominos on the Board.

--- core/src/main/java/dev/kingdomino/game/TestBoard.java
+++ core/src/main/java/dev/kingdomino/game/TestBoard.java
@@ -1,46 +1,118 @@
 package dev.kingdomino.game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestBoard {
-    public static void main(String[] args) {
-        IBoard board = new Board();
-        Tile tile = new Tile(TerrainType.WHEATFIELD, 1);
-        System.out.println(board.isTilePlaceable(tile, 4, 3));
-        System.out.println(board.isTilePlaceable(tile, 4, 4));
-        System.out.println(board.isTilePlaceable(tile, 4, 5));
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        board.setTile(tile, 4, 5);
-        board.setTile(tile, 4, 6);
-        board.setTile(tile, 4, 7);
-        board.setTile(tile, 4, 8);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Board board = new Board();
+        Domino domino = DominoDeck.DOMINO_1.getDomino();
+        boolean flag = false;
 
+        while (true) {
+            if (reader.ready()) {
+                char key = (char) reader.read();
 
-        System.out.println(board.isTilePlaceable(tile, 3, 4));
-        board.setTile(tile, 3, 4);
-        board.setTile(tile, 2, 4);
-        board.setTile(tile, 5, 4);
-        board.setTile(tile, 6, 4);
-        // board.setTile(tile, 4, 9);
-        // board.setTile(tile, 4, 10);
-        // board.setTile(tile, 4, 3);
+                switch (key) {
+                    case 'e':
+                        domino.rotate(true);
+                        break;
+                    case 'q':
+                        domino.rotate(false);
+                        break;
+                    case 'w':
+                        domino.moveDomino(Offset.UP);
+                        break;
+                    case 's':
+                        domino.moveDomino(Offset.DOWN);
+                        break;
+                    case 'a':
+                        domino.moveDomino(Offset.LEFT);
+                        break;
+                    case 'd':
+                        domino.moveDomino(Offset.RIGHT);
+                        break;
+                    case 'x':
+                        board.setDomino(domino);
+                        domino = DominoDeck.DOMINO_2.getDomino();
+                        break;
+                    case 'c':
+                        System.out.println("Exiting...");
+                        flag = true;
+                    default:
+                        break;
+                }
+            }
+            clearScreen();
 
-        System.out.println(board.isTilePlaceable(tile, 4, 3));  // out of bound for x
-        System.out.println(board.isTilePlaceable(tile, 7, 4));  // out of bound for y
-        System.out.println(board.isTilePlaceable(tile, 5, 5));  // true
+            if (flag) {
+                break;
+            }
 
-        printBoard(board);
-        // System.out.println(board.getTile(4, 5).getTerrain());
+            render(board, domino);
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void printBoard(IBoard board) {
+    public static void render(Board board, Domino domino) {
+        Tile[][] land = board.getLand(); // getLand() returns a clone of the land
+        Tile tile1 = domino.getTileA();
+        Tile tile2 = domino.getTileB();
+        Position pos1 = domino.getPosTileA();
+        Position pos2 = domino.getPosTileB();
+
+        land[(pos1.x())][(pos1.y())] = tile1;
+        land[(pos2.x())][(pos2.y())] = tile2;
+
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board.getTile(j, i) != null) {
-                    System.out.print(board.getTile(j, i).getTerrain() + " ");
+                if (land[j][i] != null) {
+                    System.out.print(getCharType(land[j][i].getTerrain()) + " ");
                 } else {
-                    System.out.print("null ");
+                    System.out.print("  ");
                 }
             }
             System.out.println();
         }
+
+        // land[pos1.x()][pos1.y()] = null;
+        // land[pos2.x()][pos2.y()] = null;
+    }
+
+    public static char getCharType(TerrainType type) {
+        switch (type) {
+            case WHEATFIELD:
+                return 'W';
+            case FOREST:
+                return 'F';
+            case LAKE:
+                return 'L';
+            case GRASSLAND:
+                return 'G';
+            case SWAMP:
+                return 'S';
+            case MINE:
+                return 'M';
+            case MOUNTAIN:
+                return 'T';
+            case DESERT:
+                return 'D';
+            case CASTLE:
+                return 'C';
+            default:
+                return ' ';
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
--- core/src/main/java/dev/kingdomino/game/TestDomino.java
+++ core/src/main/java/dev/kingdomino/game/TestDomino.java
@@ -5,24 +5,31 @@ public class TestDomino {
         IBoard board = new Board();
         Domino domino = DominoDeck.DOMINO_1.getDomino();
 
-
-        System.out.println(domino.getPosTileA());
-        System.out.println(domino.getPosTileB());
-        System.out.println("moving DOWN");
+        domino.moveDomino(Offset.RIGHT);
+        domino.moveDomino(Offset.RIGHT);
+        domino.moveDomino(Offset.RIGHT);
+        domino.moveDomino(Offset.DOWN);
+        domino.moveDomino(Offset.DOWN);
         domino.moveDomino(Offset.DOWN);
         System.out.println(domino.getPosTileA());
         System.out.println(domino.getPosTileB());
-        // System.out.println(board.isDominoPlaceable(domino));
 
-        System.out.println("rotating clockwise");
-        domino.rotate(true);
-        System.out.println(domino.getPosTileA());
-        System.out.println(domino.getPosTileB());
+        System.out.println(board.isDominoPlaceable(domino));
+        
+        // System.out.println("moving DOWN");
+        // domino.moveDomino(Offset.DOWN);
+        // System.out.println(domino.getPosTileA());
+        // System.out.println(domino.getPosTileB());
 
-        System.out.println("rotating clockwise");
-        domino.rotate(true);
-        System.out.println(domino.getPosTileA());
-        System.out.println(domino.getPosTileB());
+        // System.out.println("rotating clockwise");
+        // domino.rotate(true);
+        // System.out.println(domino.getPosTileA());
+        // System.out.println(domino.getPosTileB());
+
+        // System.out.println("rotating clockwise");
+        // domino.rotate(true);
+        // System.out.println(domino.getPosTileA());
+        // System.out.println(domino.getPosTileB());
     }
 
     public static void printBoard(IBoard board) {
