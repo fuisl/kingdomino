e4504671c9de8e60dda8b118b6e6d03531d012f8
fuisl <135719107+fuisl@users.noreply.github.com>
2024-12-21 15:51:39 +0100

    refactor: encapsulate Offset to enum.

---
+++ core/src/main/java/dev/kingdomino/game/Offset.java
@@ -0,0 +1,18 @@
+package dev.kingdomino.game;
+
+public enum Offset {
+    RIGHT(1, 0),
+    DOWN(0, 1),
+    LEFT(-1, 0),
+    UP(0, -1);
+
+    private Position position;
+
+    Offset(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
+    public Position get() {
+        return position;
+    }
+}
--- core/src/main/java/dev/kingdomino/game/TileRotator.java
+++ core/src/main/java/dev/kingdomino/game/TileRotator.java
@@ -1,11 +1,11 @@
 package dev.kingdomino.game;
 
 public class TileRotator implements ITileRotator {
-    private final Position[] offsets = {
-            new Position(1, 0), // right
-            new Position(0, 1), // down
-            new Position(-1, 0), // left
-            new Position(0, -1) // up
+    private static final Position[] offsets = {
+            Offset.RIGHT.get(),
+            Offset.DOWN.get(),
+            Offset.LEFT.get(),
+            Offset.UP.get()
     };
 
     @Override
