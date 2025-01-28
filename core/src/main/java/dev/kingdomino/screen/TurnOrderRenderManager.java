2470a184dec1e6819a4b80aacefe9636d3a9ac2c
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-01-28 10:14:16 +0000

    fix: nextTurnIndex pointing to the same King the entire time

--- core/src/main/java/dev/kingdomino/screen/TurnOrderRenderManager.java
+++ core/src/main/java/dev/kingdomino/screen/TurnOrderRenderManager.java
@@ -66,6 +66,8 @@ public class TurnOrderRenderManager extends AbstractRenderManager {
             while (nextTurnOrder[nextTurnIndex] == null) {
                 nextTurnIndex++;
 
+                // failsafe in case somehow there are less than kingCount in the order queue,
+                // which should be impossible.
                 if (nextTurnIndex == kingCount) {
                     for (; i < kingCount; i++) {
                         playerIconActors[i].setKingID(-1);
@@ -76,6 +78,7 @@ public class TurnOrderRenderManager extends AbstractRenderManager {
             }
 
             playerIconActors[i].setKingID(nextTurnOrder[nextTurnIndex].getId());
+            nextTurnIndex++;
         }
     }
 }
